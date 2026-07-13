d9de684269aff17a5c94c19f1d8bddc80a058d32
alentandara <alen2211@gmail.com>
2026-07-13 18:55:16 +0200

    javascript

---
+++ 03_js/vjezba01/skripta.js
@@ -0,0 +1,9 @@
+//alert('Hello world1')
+
+
+document.getElementById('ispis').innerText='Hello world6'
+document.getElementById('ispis').style.color='red'
+
+
+document.getElementById('ispis').innerHTML=
+'Hello world6'
